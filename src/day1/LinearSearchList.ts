69cf8b579bb84f3e9c100bb5512e50385764c7cd
leontoys <svliyon@gmail.com>
2025-02-08 08:17:52 +1100

    dsa-kata-machine

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,11 @@
+export default function linear_search(haystack: number[], needle: number): boolean {
+    return true
+    for (let index = 0; index < haystack.length; index++) {
+        //const element = ;
+        if (haystack[index]===needle) {
+            return true
+        }
+        
+    }
+    return false
+}
